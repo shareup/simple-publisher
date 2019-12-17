45de19e78860b65eb5d98f62e90de01a9af98e24
Nathan Herald <me@nathanherald.com>
2019-12-17 09:12:32 +0100

    The Coordinator is not needed

--- Sources/SimplePublisher/SimplePublisher.swift
+++ Sources/SimplePublisher/SimplePublisher.swift
@@ -1,19 +1,62 @@
 import Foundation
 import Combine
+import Synchronized
 
-public class SimplePublisher<Output, Failure: Error>: Publisher {
+public class SimplePublisher<Output, Failure: Error>: Publisher, Synchronized {
     public typealias Output = Output
     public typealias Failure = Failure
     
-    private let coordinator = SimpleCoordinator<Output, Failure>()
+    var subscriptions: [SimpleSubscription<Output, Failure>] = []
+    var isComplete = false
+    var isIncomplete: Bool { return !isComplete }
     
     public func receive<S>(subscriber: S)
         where S: Subscriber, Failure == S.Failure, Output == S.Input {
-
-        coordinator.receive(subscriber: subscriber)
+        sync {
+            guard isIncomplete else { return }
+        }
+        
+        let subscription = SimpleSubscription(publisher: self.eraseToAnyPublisher(), subscriber: AnySubscriber(subscriber))
+        subscriber.receive(subscription: subscription)
+        
+        sync {
+            subscriptions.append(subscription)
+        }
     }
     
     func publish(_ output: Output) {
-        coordinator.receive(output)
+        sync {
+            guard isIncomplete else { return }
+            
+            for subscription in subscriptions {
+                subscription.receive(output)
+            }
+        }
+    }
+    
+    func complete() {
+        complete(.finished)
+    }
+    
+    func complete(_ failure: Failure) {
+        complete(.failure(failure))
+    }
+    
+    func complete(_ completion: Subscribers.Completion<Failure>) {
+        sync {
+            guard isIncomplete else { return }
+            
+            for subscription in subscriptions {
+                subscription.receive(completion: completion)
+            }
+            
+            subscriptions.removeAll()
+            
+            self.isComplete = true
+        }
+    }
+    
+    deinit {
+        subscriptions.removeAll()
     }
 }
