7d79cee71877915475a3fb63eb6bc67cd9d6b07c
Nathan Herald <me@nathanherald.com>
2019-12-17 09:21:12 +0100

    Additional test which tests cancelling mid-test and starting late and not getting historical items

--- Tests/SimplePublisherTests/SimplePublisherTests.swift
+++ Tests/SimplePublisherTests/SimplePublisherTests.swift
@@ -34,7 +34,7 @@ final class SimplePublisherTests: XCTestCase {
         
         let ex = expectation(description: "Should get a string in about 2 seconds")
 
-        let sub = emitter.forever { string in
+        let sub = emitter.forever { _ in
             ex.fulfill()
         }
         defer { sub.cancel() }
@@ -50,7 +50,7 @@ final class SimplePublisherTests: XCTestCase {
         let ex = expectation(description: "Should get a 2 strings in about 4 seconds")
         ex.expectedFulfillmentCount = 2
 
-        let sub = emitter.forever { string in
+        let sub = emitter.forever { _ in
             ex.fulfill()
         }
         defer { sub.cancel() }
@@ -66,7 +66,7 @@ final class SimplePublisherTests: XCTestCase {
         let ex = expectation(description: "Should get a 3 strings in about 6 seconds")
         ex.expectedFulfillmentCount = 3
 
-        let sub = emitter.forever { string in
+        let sub = emitter.forever { _ in
             ex.fulfill()
         }
         defer { sub.cancel() }
@@ -83,16 +83,52 @@ final class SimplePublisherTests: XCTestCase {
         let foreverEx = expectation(description: "Forever should get 2 strings in about 4 seconds")
         foreverEx.expectedFulfillmentCount = 2
         
-        let sub1 = emitter.sink { string in
+        let sub1 = emitter.sink { _ in
             sinkEx.fulfill()
         }
         defer { sub1.cancel() }
         
-        let sub2 = emitter.forever { string in
+        let sub2 = emitter.forever { _ in
             foreverEx.fulfill()
         }
         defer { sub2.cancel() }
         
         waitForExpectations(timeout: 4.1)
+    }
+    
+    func testEmitterHasDifferentSubscribersOverTime() {
+        var emitter = Emitter()
+        emitter.start()
+        defer { emitter.stop() }
+        
+        let sink1Ex = expectation(description: "Sink1 should get 1 string only")
+        let sink2Ex = expectation(description: "Sink2 should get 1 string only")
+        let forever1Ex = expectation(description: "Forever1 should get 1 string only in about 2 seconds")
+        let forever2Ex = expectation(description: "Forever2 should get 1 string only since it starts late and shouldn't get historical items")
+        
+        let sub1 = emitter.sink { _ in
+            sink1Ex.fulfill()
+        }
+        
+        let sub2 = emitter.forever { _ in
+            forever1Ex.fulfill()
+        }
+        
+        wait(for: [sink1Ex], timeout: 2.1)
+        
+        sub1.cancel()
+        sub2.cancel()
+        
+        let sub3 = emitter.sink { _ in
+            sink2Ex.fulfill()
+        }
+        defer { sub3.cancel() }
+        
+        let sub4 = emitter.forever { _ in
+            forever2Ex.fulfill()
+        }
+        defer { sub4.cancel() }
+        
+        waitForExpectations(timeout: 2.1)
     }
 }
